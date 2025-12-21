59ce11a36985a09a5fb1de105267ad5ba67b8fc2
Luca Deeken <LDeeken@aol.com>
2025-12-21 16:40:52 +0100

    feat: cartArea that shows articles in cart.

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,15 +1,27 @@
 import styles from "./Cart.module.css";
 import { useState, useEffect, useContext } from "react";
 import { ProductsContext } from "../../context/ProductsContext";
+import CartProduct from "./CartProduct";
 
 function Cart() {
-  const { cart } = useContext(ProductsContext);
+  const { cart, setCart } = useContext(ProductsContext);
 
-  console.log(cart);
   return (
     <>
       <main className={styles.outerLayout}>
-        <section className={styles.shoppingBag}></section>
+        <section className={styles.shoppingBag}>
+          {cart.map((object) => (
+            <CartProduct
+              object={object.product}
+              name={object.product.title}
+              img={object.product.images[0]}
+              amount={object.quantity}
+              price={object.product.price}
+              setCart={setCart}
+              cart={cart}
+            />
+          ))}
+        </section>
         <section className={styles.orderSummary}></section>
       </main>
     </>
--- src/pages/Cart/CartProduct.jsx
+++ src/pages/Cart/CartProduct.jsx
@@ -1,26 +1,37 @@
 import styles from "./CartProduct.module.css";
+import { useState, useEffect, useContext } from "react";
 
-function CartProduct() {
-  const { cart } = useContext(ProductsContext);
-
-  const [articleAmount, changeArticleAmount] = useState(0);
-
-  function increaseArticleNum() {
-    setArticleToCart((prev) => prev + 1);
+function CartProduct({ object, name, img, amount, price, setCart, cart }) {
+  function increaseArticleNum(object) {
+    setCart(
+      cart.map((item) =>
+        item.product.id === object.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
   }
 
-  function decreaseArticleNum() {
-    setArticleToCart((prev) => (prev > 0 ? prev - 1 : 0));
+  function decreaseArticleNum(object) {
+    if (amount < 1) {
+      return;
+    } else {
+      setCart(
+        cart.map((item) =>
+          item.product.id === object.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+      );
+    }
   }
-
-  console.log(cart);
+  console.log(object);
   return (
     <>
-      <article>
-        <img src="" alt="" />
+      <article className={styles.article}>
+        <img src={img} alt={name} className={styles.image} />
         <div>
-          <h2>name</h2>
-          <p>amount5</p>
+          <h2>{name}</h2>
         </div>
 
         <div className={styles.inputContainer}>
@@ -28,7 +39,7 @@ function CartProduct() {
             type="button"
             className={styles.minusBtn}
             onClick={() => {
-              decreaseArticleNum();
+              decreaseArticleNum(object);
             }}
           >
             –
@@ -37,24 +48,24 @@ function CartProduct() {
             type="num"
             id="article"
             className={styles.input}
-            value={articleAmount}
+            value={amount}
             readOnly
           />
           <button
             type="button"
             className={styles.plusBtn}
             onClick={() => {
-              increaseArticleNum();
+              increaseArticleNum(object);
             }}
           >
             +
           </button>
         </div>
         <button>delete</button>
-        <p>price</p>
+        <p>{price}</p>
       </article>
     </>
   );
 }
 
-export default Cart;
+export default CartProduct;
